055388143a97d826de80ab942fa1cd5cdf2053e0
vtsoma <cheesetkh39@gmail.com>
2023-05-23 13:14:25 +0300

    Initial commit

---
+++ RegistryDestroyer/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Win32;
+
+RegistryKey Root =  Registry.ClassesRoot;
+List<string> matches = new List<string>{};
+
+foreach (string subkeyName in Root.GetSubKeyNames())
+{
+    if (subkeyName.Substring(0, 1).Equals("."))
+    {
+        matches.Add(subkeyName);
+    }
+}
+
+foreach (string subkeyName in matches)
+{
+    Console.WriteLine(subkeyName);
+}
